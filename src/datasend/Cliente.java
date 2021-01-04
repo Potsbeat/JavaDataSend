f20bfbb6b6a1e50ceec8709e486987cf061446ac
Potsbeat <bichillocarmon@gmail.com>
2021-01-04 15:10:59 -0600

    First Commit

---
+++ src/datasend/Cliente.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datasend;
+
+import javax.swing.JFileChooser;
+import java.net.*;
+import java.io.*;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
+public class Cliente {
+
+    boolean done = false;
+    Progreso progreso;
+    long enviados, enviados2, total;
+    int n;
+    int porcentaje, porcentaje2;
+
+    public Cliente(String host, int port, int buff_size, boolean nagle) {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setMultiSelectionEnabled(true);
+        int r = chooser.showOpenDialog(null);
+        if (r == JFileChooser.APPROVE_OPTION) {
+            conectar(host, port, buff_size, nagle, chooser.getSelectedFiles());
+        }
+
+    }
+
+    public static String humanReadableByteCountSI(long bytes) {
+        if (-1000 < bytes && bytes < 1000) {
+            return bytes + " B";
+        }
+        CharacterIterator ci = new StringCharacterIterator("kMGTPE");
+        while (bytes <= -999_950 || bytes >= 999_950) {
+            bytes /= 1000;
+            ci.next();
+        }
+        return String.format("%.1f %cB", bytes / 1000.0, ci.current());
+    }
+
+    public void conectar(String host, int port, int buff_size, boolean nagle, File[] f) {
+        try {
+
+            Socket cl = new Socket(host, port);
+
+            cl.setTcpNoDelay(!nagle); //Activa o desactiva el algoritmo de Nagle
+
+            byte[] b = new byte[buff_size];
+
+            DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+
+            dos.writeInt(buff_size);
+            dos.flush();
+            dos.write(f.length);
+            dos.flush();
+
+            progreso = new Progreso();
+
+            total = 0;
+            for (File file : f) {
+                progreso.appendRow(file.getName(), humanReadableByteCountSI(file.length()));
+                //progreso.listAppend(file.getName() + " " + humanReadableByteCountSI(file.length()) + "\n");
+                total += file.length();
+            }
+            progreso.update(progreso.getGraphics());
+            new SwingWorker() {
+                @Override
+                protected Object doInBackground() throws Exception {
+                    enviados2 = 0;
+                    for (File file : f) {
+                        enviados = 0;
+
+                        DataInputStream dis = new DataInputStream(new FileInputStream(file.getAbsolutePath()));
+                        dos.writeUTF(file.getName());
+                        dos.flush();
+                        dos.writeLong(file.length());
+                        dos.flush();
+
+                        progreso.enviandoSetLabel("Enviando: " + file.getName());
+
+                        while (enviados < file.length()) {
+                            n = dis.read(b);
+                            dos.write(b, 0, n);
+                            dos.flush();
+
+                            enviados = enviados + n;
+                            enviados2 = enviados2 + n;
+                            porcentaje = (int) (enviados * 100 / file.length());
+                            System.out.print("Enviado: " + porcentaje + "%\r");
+
+                            progreso.setIndBarValue(porcentaje);
+                            progreso.getProgreso_individual_bar().setString(porcentaje + "%");
+
+                            porcentaje2 = (int) (enviados2 * 100 / total);
+                            progreso.setGenBarValue(porcentaje2);
+                            progreso.getProgreso_general_bar().setString(porcentaje2 + "%");
+
+                        }//While
+
+                        System.out.print("\n\nArchivo enviado");
+                        dis.close();
+
+                    }
+
+                    dos.close();
+                    cl.close();
+                    return null;
+
+                }
+            }.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/datasend/Servidor.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datasend;
+
+import java.net.*;
+import java.io.*;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+        try {
+            // Creamos el socket
+            ServerSocket s = new ServerSocket(7000);
+            // Iniciamos el ciclo infinito del servidor
+            for (;;) {
+                // Esperamos una conexión 
+                Socket cl = s.accept();
+                System.out.println("Conexión establecida desde" + cl.getInetAddress() + ":" + cl.getPort());
+                DataInputStream dis = new DataInputStream(cl.getInputStream());
+                
+                int buff_size = dis.readInt();
+                int n_archivos = dis.read();
+                System.out.println("Se recibirán " + n_archivos + " archivos con un buff de "+buff_size);
+                byte[] b = new byte[buff_size];
+                for (int i = 0; i < n_archivos; i++) {
+                    
+                    String nombre = dis.readUTF();
+                    System.out.println("Recibimos el archivo:" + nombre);
+                    long tam = dis.readLong();
+                    DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre));
+                    long recibidos = 0;
+                    int n, porcentaje;
+                    while (recibidos < tam) {
+                        n = dis.read(b);
+                        dos.write(b, 0, n);
+                        dos.flush();
+                        recibidos = recibidos + n;
+                        porcentaje = (int) (recibidos * 100 / tam);
+                        System.out.print("Recibido: " + porcentaje + "%\r");
+                    }//While
+                    System.out.print("\n\nArchivo recibido.\n");
+                    dos.close();
+                }
+                dis.close();
+                cl.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }//catch
+    }
+}
